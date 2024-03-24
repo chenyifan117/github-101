de6611b4576d9a7fcf041587c16f134b78d09769
Yifan Chen <chenyifan@wustl.edu>
2024-03-24 15:39:16 -0500

    add name parameter

--- second_file.py
+++ second_file.py
@@ -1,8 +1,8 @@
 def print_hello_world():
     print("Hello World")
 
-def print_my_name():
-    print("Hi, my name is xxx")
+def print_my_name(name):
+    print(f"Hi, my name is {name}")
 
 print_hello_world()
 print_my_name()
